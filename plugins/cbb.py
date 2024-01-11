6b61c1426504ade0464c72e21a0f3f21ac42a22d
Shid <156281705+ShidBot02@users.noreply.github.com>
2024-01-11 20:47:55 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/notanimechidori'>Bucker ©</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {_version_}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel 1🧏 : <a href='https://t.me/animechidori'>Anime Chidori</a>\n○ Channel 2💯 : <a href='https://t.me/ongoing_anime'>Ongoing Anime</a></b>",
+            text = f"<b>○ Creator : <a href='https://t.me/notanimechidori'>Bucker ©</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel 1🧏 : <a href='https://t.me/animechidori'>Anime Chidori</a>\n○ Channel 2💯 : <a href='https://t.me/ongoing_anime'>Ongoing Anime</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
