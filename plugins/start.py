f9deb346e4581431b1d8715b53b1d91a6fdee709
Shid <156281705+ShidBot02@users.noreply.github.com>
2024-03-24 22:21:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,7 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
-
+PIC = "https://graph.org//file/2b5d9562e575859a040a3.jpg"
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -94,8 +94,9 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
+        await message.reply_photo(
+            photo = PIC,
+            caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
@@ -103,8 +104,8 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
+            #disable_web_page_preview = True,
+            #quote = True
         )
         return
 
